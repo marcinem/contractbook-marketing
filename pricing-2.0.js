76e4b9201446bd43fec645072dd639dfdbce2163
Marcin <marcinmichalak.m@gmail.com>
2022-11-07 20:47:22 +0100

    udpated geolocation pricing script

--- pricing-2.0.js
+++ pricing-2.0.js
@@ -5,16 +5,15 @@ const gbpSwitch = document.getElementById("price-gbp");
 function detectLocation() {
   /////// Detect country and change to local currency
   var countryCode = sessionStorage.getItem("clientLoc");
-  var usdCountries = ["US", "CA", "IL", "MX"];
-  var dkkCountries = ["DK"];
+  var euCountries = ["AT","BE","BG","HR","CY","CZ","DK","EE","FI","FR","DE","GR","HU","IE","IT","LV","LT","LU","MT","NL","PL","PT","RO","SK","SI","ES","SE","EU","IS","LI","NO","CH"];
   var gbpCountries = ["GB"];
 
-  if (usdCountries.includes(countryCode)) {
-    usdSwitch.click();
+  if (euCountries.includes(countryCode)) {
+    eurSwitch.click();
   } else if (gbpCountries.includes(countryCode)) {
     gbpSwitch.click();
   } else {
-    eurSwitch.click();
+    usdSwitch.click();
   }
 }
 
