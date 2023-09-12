7206f39b30271bcede285120f872d2e31c19836c
Marcin <marcinmichalak.m@gmail.com>
2023-09-12 14:25:15 +0200

    VWO Improvements

--- vwo-tracking.js
+++ vwo-tracking.js
@@ -1,54 +1,73 @@
-const getCookie = (name) => {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  return parts.length === 2 ? parts.pop().split(";").shift() : null;
-};
+const MAX_RETRIES = 3;
+const RETRY_DELAY = 2000; // 2 second delay
 
-const checkVWO = () => {
-  if (getCookie("_vis_opt_out")) {
-    console.log("::: Opted out of VWO");
-    return;
+window._vis_opt_queue = window._vis_opt_queue || [];
+window._vis_opt_queue.push(function () {
+  const checkVWO = () => {
+    if (getCookie("_vis_opt_out")) {
+      console.log("::: Opted out of VWO");
+      return;
+    }
+
+    if (window._vwo_exp_ids) {
+      captureExperiments();
+      checkHubspotFormAndRun();
+      sendEventsToPlausible();
+    } else {
+      setTimeout(checkVWO, 2000);
+    }
+  };
+
+  checkVWO();
+});
+
+const captureExperiments = (retryCount = 0) => {
+  const experiments = window._vwo_exp_ids;
+  let shouldRetry = false;
+  let newData = {};
+
+  if (experiments) {
+    for (let id of experiments) {
+      const experiment = window._vwo_exp[id];
+
+      if (experiment) {
+        const combinationNum = experiment.combination_chosen;
+        const variationName = experiment.comb_n[combinationNum];
+
+        if (!variationName) {
+          shouldRetry = true;
+          continue;
+        }
+
+        if (experiment.status === "RUNNING") {
+          newData[id] = {
+            id,
+            name: experiment.name,
+            type: experiment.type,
+            variation: variationName,
+          };
+        }
+      }
+    }
+
+    if (shouldRetry && retryCount < MAX_RETRIES) {
+      return setTimeout(() => captureExperiments(retryCount + 1), RETRY_DELAY);
+    }
   }
 
-  captureExperiments();
-  checkHubspotFormAndRun();
-  sendEventsToPlausible();
-};
+  // Merge existing data with new data
+  const existingData =
+    JSON.parse(localStorage.getItem("vwo_experiments")) || [];
+  const existingDataMap = Object.fromEntries(
+    existingData.map((e) => [e.id, e]),
+  );
 
-const captureExperiments = () => {
-  window.VWO = window.VWO || [];
-  let newData = {};
-  window.VWO.push([
-    "onVariationApplied",
-    function (data) {
-      if (!data) {
-        return;
-      }
-      var expId = data[1],
-        variationId = data[2];
-      if (typeof _vwo_exp[expId].comb_n[variationId] !== "undefined") {
-        newData[expId] = {
-          expId,
-          name: _vwo_exp[expId].name,
-          variation: _vwo_exp[expId].comb_n[variationId],
-        };
-      }
-      const existingData =
-        JSON.parse(localStorage.getItem("vwo_experiments")) || [];
-      const existingDataMap = Object.fromEntries(
-        existingData.map((e) => [e.id, e]),
-      );
+  // Overwrite or add new experiments
+  Object.assign(existingDataMap, newData);
 
-      // Overwrite or add new experiments
-      Object.assign(existingDataMap, newData);
-
-      // Convert back to array and store in localStorage
-      const mergedData = Object.values(existingDataMap);
-      localStorage.setItem("vwo_experiments", JSON.stringify(mergedData));
-      //test
-      console.log(localStorage.getItem("vwo_experiments"));
-    },
-  ]);
+  // Convert back to array and store in localStorage
+  const mergedData = Object.values(existingDataMap);
+  localStorage.setItem("vwo_experiments", JSON.stringify(mergedData));
 };
 
 const updateVwoHubspotFields = () => {
@@ -101,4 +120,3 @@ const sendEventsToPlausible = () => {
     }, 4000);
   });
 };
-checkVWO();
