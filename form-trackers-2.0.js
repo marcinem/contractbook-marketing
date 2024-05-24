041c3b3e380775c4a883b1ac63d54bb2ba719a52
Marcin <marcinmichalak.m@gmail.com>
2024-05-24 09:03:35 +0200

    Update form-trackers-2.0.js

--- form-trackers-2.0.js
+++ form-trackers-2.0.js
@@ -16,102 +16,113 @@ window.addEventListener("CookieScriptLoaded", () => {
       fillForms();
     }
   });
-});
 
-function buildTrackingJar() {
-  return new Promise((resolve, reject) => {
-    // check Facebook cookies
-    (function lookForFbp(i) {
-      if (!fbpCookie) {
-        setTimeout(function () {
-          fbpCookie = Cookies.get("_fbp");
-          fbcCookie = Cookies.get("_fbc");
-          if (--i) {
-            lookForFbp(i);
-          } else {
-            getKnownParams();
-            resolve();
-          }
-        }, 2000);
+
+  function buildTrackingJar() {
+    return new Promise((resolve, reject) => {
+      // check Facebook cookies
+      (function lookForFbp(i) {
+        if (!fbpCookie) {
+          setTimeout(function () {
+            fbpCookie = Cookies.get("_fbp");
+            fbcCookie = Cookies.get("_fbc");
+            if (--i) {
+              lookForFbp(i);
+            } else {
+              getKnownParams();
+              resolve();
+            }
+          }, 2000);
+        } else {
+          getKnownParams();
+          resolve();
+        }
+      })(3);
+    });
+  }
+  // comment
+  function getKnownParams() {
+    trackingParams["fb_browser_id"] = fbpCookie || "";
+    // validation of Fb Clid cookie
+    var timestamp = trackingParams.timestamp;
+    // Cookie exists && has correct formatting
+    if (fbcCookie) {
+      if (fbcCookie.includes("fb.")) {
+        trackingParams["fbclid"] = fbcCookie;
       } else {
-        getKnownParams();
-        resolve();
+        // Cookie exists but has wrong formatting
+        trackingParams["fbclid"] = "fb.1." + timestamp + "." + fbcCookie;
       }
-    })(3);
-  });
-}
-// comment
-function getKnownParams() {
-  trackingParams["fb_browser_id"] = fbpCookie || "";
-  // validation of Fb Clid cookie
-  var timestamp = trackingParams.timestamp;
-  // Cookie exists && has correct formatting
-  if (fbcCookie) {
-    if (fbcCookie.includes("fb.")) {
-      trackingParams["fbclid"] = fbcCookie;
-    } else {
-      // Cookie exists but has wrong formatting
-      trackingParams["fbclid"] = "fb.1." + timestamp + "." + fbcCookie;
+      // Cookie DOESNT exist && param exists and it's not empty, && param has wrong formatting
+    } else if (!fbcCookie && trackingParams.fbclid) {
+      if (
+        trackingParams.fbclid !== "" &&
+        !trackingParams.fbclid.includes("fb.")
+      ) {
+        trackingParams.fbclid = "fb.1." + timestamp + "." + trackingParams.fbclid;
+      }
     }
-    // Cookie DOESNT exist && param exists and it's not empty, && param has wrong formatting
-  } else if (!fbcCookie && trackingParams.fbclid) {
-    if (
-      trackingParams.fbclid !== "" &&
-      !trackingParams.fbclid.includes("fb.")
-    ) {
-      trackingParams.fbclid = "fb.1." + timestamp + "." + trackingParams.fbclid;
+
+    // get gclid from the local storage
+    if (sessionStorage.getItem("gclid")) {
+      trackingParams["gclid"] = sessionStorage.getItem("gclid");
     }
+    if (localStorage.getItem("lp")) {
+      trackingParams["landing_page"] = localStorage.getItem("lp");
+    }
+
+    // save information about convertion page
+    trackingParams["conversion_page"] = window.location.href;
+
+    // get IP from the local storage
+    if (sessionStorage.getItem("clientIp")) {
+      trackingParams["ip_address_form"] = sessionStorage.getItem("clientIp");
+    }
+
+    // get country from the local storage
+    if (sessionStorage.getItem("clientCountry")) {
+      trackingParams["country_contact"] = sessionStorage.getItem("clientCountry");
+    }
+
+    // get user agent
+    trackingParams["client_user_agent"] = window.navigator.userAgent;
+
+    // Facebook event unique ID
+    var fbEventId = ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+      (
+        c ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+      ).toString(16),
+    );
+
+    trackingParams["facebook_event_id"] = fbEventId;
+
+    trackingParams["rudderstack_anonymous_id"] = rudderanalytics.getAnonymousId();
+
+    (function waitForAnalyticsUser(i) {
+      if (typeof analytics !== "undefined" && analytics.user && analytics.user().anonymousId) {
+        trackingParams["segment_anonymous_id"] = analytics.user().anonymousId();
+      } else if (--i > 0) {
+        setTimeout(() => waitForAnalyticsUser(i), 1000);
+      } else {
+        console.error("Segment analytics not loaded in time");
+      }
+    })(10);
+    // trackingParams["segment_anonymous_id"] = analytics.user().anonymousId();
   }
 
-  // get gclid from the local storage
-  if (sessionStorage.getItem("gclid")) {
-    trackingParams["gclid"] = sessionStorage.getItem("gclid");
-  }
-  if (localStorage.getItem("lp")) {
-    trackingParams["landing_page"] = localStorage.getItem("lp");
-  }
+  // fill all the fields
 
-  // save information about convertion page
-  trackingParams["conversion_page"] = window.location.href;
-
-  // get IP from the local storage
-  if (sessionStorage.getItem("clientIp")) {
-    trackingParams["ip_address_form"] = sessionStorage.getItem("clientIp");
-  }
-
-  // get country from the local storage
-  if (sessionStorage.getItem("clientCountry")) {
-    trackingParams["country_contact"] = sessionStorage.getItem("clientCountry");
-  }
-
-  // get user agent
-  trackingParams["client_user_agent"] = window.navigator.userAgent;
-
-  // Facebook event unique ID
-  var fbEventId = ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-    (
-      c ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-    ).toString(16),
-  );
-
-  trackingParams["facebook_event_id"] = fbEventId;
-
-  trackingParams["rudderstack_anonymous_id"] = rudderanalytics.getAnonymousId();
-  trackingParams["segment_anonymous_id"] = analytics.user().anonymousId();
-}
-
-// fill all the fields
-
-async function fillForms() {
-  await buildTrackingJar();
-  for (const [key, value] of Object.entries(trackingParams)) {
-    var fields = document.querySelectorAll(`[name="${key}"]`);
-    if (fields.length) {
-      fields.forEach(function (field) {
-        field.value = value;
-        field.dispatchEvent(new Event('input', { bubbles: true }));
-      });
+  async function fillForms() {
+    await buildTrackingJar();
+    for (const [key, value] of Object.entries(trackingParams)) {
+      var fields = document.querySelectorAll(`[name="${key}"]`);
+      if (fields.length) {
+        fields.forEach(function (field) {
+          field.value = value;
+          field.dispatchEvent(new Event('input', { bubbles: true }));
+        });
+      }
     }
   }
-}
+});
